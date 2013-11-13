2b92b651d0347dd8a3bfa48a420878374fcbb17e
angel.s.dimitrov@gmail.com <angel.s.dimitrov@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-13 11:52:09 +0000

    Test upload

--- trunk/TeamRubellite/ToDo/Program.cs
+++ trunk/TeamRubellite/ToDo/Program.cs
@@ -11,6 +11,8 @@
         static void Main()
         {
             // this is it
+            DateTime dt = new DateTime(2013, 11, 13, 13,50,0);
+            Console.WriteLine("Test upload " + dt);
         }
     }
 }
