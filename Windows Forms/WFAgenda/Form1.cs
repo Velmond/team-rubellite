cef419d8feaee8f29383b29c320ea6173f382dd0
rstoqnow@gmail.com <rstoqnow@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-17 08:36:05 +0000

    - Updated XAML project with the BirthdayReminder.cs class  - Minor updates for the Win Forms project (the UI is affected)

--- Windows Forms/WFAgenda/Form1.cs
+++ Windows Forms/WFAgenda/Form1.cs
@@ -8,11 +8,26 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using AgendaBackend;
+using TodoListConsole;
 
 namespace WFAgenda
 {
     public partial class Form1 : Form
     {
+        private List<AgendaObject> listOfAgendaObjects;
+
+        public List<AgendaObject> ListOfAgendaObjects
+        {
+            get
+            {
+                return this.listOfAgendaObjects;
+            }
+            set
+            {
+                this.listOfAgendaObjects = value;
+            }
+        }
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,7 +37,7 @@ namespace WFAgenda
         {
             comboBoxPriority.Enabled = false;
             comboBoxExtDuration.Enabled = false;
-            textBoxEnd.Enabled = false;
+            this.listOfAgendaObjects = new List<AgendaObject>();
         }
 
         private void tabPage6_Click(object sender, EventArgs e)
@@ -32,19 +47,94 @@ namespace WFAgenda
 
         private void comboBoxTaskType_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBoxTaskType.SelectedItem != "Note" &&
-                comboBoxTaskType.SelectedItem != "")
-            {
-                comboBoxPriority.Enabled = true;
-                comboBoxExtDuration.Enabled = true;
-                textBoxEnd.Enabled = true;
 
-            }
-            else
+            //Goal
+            //Meeting
+            //Note
+            //Task
+            //Birthday
+
+            if (comboBoxTaskType.SelectedItem == "")
             {
-                comboBoxPriority.Enabled = false;
+                groupBoxBirthday.Visible = false;
+                groupBoxEnd.Visible = false;
+                groupBoxStart.Visible = false;
                 comboBoxExtDuration.Enabled = false;
-                textBoxEnd.Enabled = false;
+                comboBoxPriority.Enabled = false;
+                comboBoxExtDuration.Visible = false;
+                comboBoxPriority.Visible = false;
+                label4.Visible = false;
+                label6.Visible = false;
+
+                this.Size = new Size(437, 375);
+                
+            }
+            else if (comboBoxTaskType.SelectedItem == "Birthday")
+            {
+                this.Size = new Size(792, 375);
+                groupBoxEnd.Visible = false;
+                groupBoxStart.Visible = false;
+                groupBoxBirthday.Visible = true;
+
+                groupBoxBirthday.Location = new Point(6, 19);
+            }
+            else if (comboBoxTaskType.SelectedItem == "Goal")
+            {
+                this.Size = new Size(792, 375);
+                groupBoxEnd.Visible = false;
+                groupBoxStart.Visible = false;
+                groupBoxBirthday.Visible = false;
+                comboBoxExtDuration.Enabled = true;
+                comboBoxPriority.Enabled = true;
+                comboBoxPriority.Visible = true;
+                comboBoxExtDuration.Visible = true;
+
+                label4.Visible = true;
+                label6.Visible = true;
+                label4.Location = new Point(16, 19);
+                label6.Location = new Point(16, 50);
+                comboBoxPriority.Location = new Point(120, 19);
+                comboBoxExtDuration.Location = new Point(120, 50);
+            }
+            else if (comboBoxTaskType.SelectedItem == "Meeting")
+            {
+                this.Size = new Size(792, 375);
+                groupBoxStart.Visible = true;
+                groupBoxStart.Location = new Point(6, 19);
+
+                groupBoxEnd.Visible = true;
+                groupBoxEnd.Location = new Point(6, 113);
+
+                groupBoxBirthday.Visible = false;
+                label4.Visible = false;
+                label6.Visible = false;
+            }
+            else if (comboBoxTaskType.SelectedItem == "Note")
+            {
+                this.Size = new Size(437,375);
+                groupBoxBirthday.Visible = false;
+                groupBoxEnd.Visible  = false;
+                groupBoxStart.Visible  = false;
+                comboBoxExtDuration.Enabled = false;
+                comboBoxPriority.Enabled = false;
+                comboBoxExtDuration.Visible = false;
+                comboBoxPriority.Visible = false;
+                label4.Visible = false;
+                label6.Visible = false;
+            }
+            else if (comboBoxTaskType.SelectedItem == "Task")
+            {
+                this.Size = new Size(792, 375);
+                label4.Visible = true;
+                label4.Location = new Point(16, 19);
+                comboBoxPriority.Visible = true;
+                comboBoxPriority.Enabled = true;
+                comboBoxPriority.Location = new Point(120, 19);
+                groupBoxBirthday.Visible = false;
+                groupBoxEnd.Visible = false;
+                comboBoxExtDuration.Visible = false;
+                comboBoxExtDuration.Enabled = false;
+                label6.Visible = false;
             }
         }
 
@@ -55,7 +145,6 @@ namespace WFAgenda
             comboBoxTaskType.SelectedIndex = 0;
             comboBoxExtDuration.SelectedIndex = 0;
             comboBoxPriority.SelectedIndex = 0;
-            textBoxEnd.Text = "";
         }
 
         private void buttonSortByDateCreated_Click(object sender, EventArgs e)
@@ -71,7 +160,136 @@ namespace WFAgenda
 
         }
 
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            this.listOfAgendaObjects.Add(AddNewAgenda(comboBoxTaskType.Text));
+            
+        }
 
+        public AgendaObject AddNewAgenda(string agendaType)
+        {
+            switch (agendaType)
+            {
+                case "Goal":
+                    {
+                        switch (comboBoxPriority.SelectedIndex)
+                        {
+                            case 0:
+                                break;
+                            case 1:
+                                {
+                                    return new Goal(textBoxTitle.Text, richTextBoxDescription.Text, Priority.None);
+                                }
+                            case 2:
+                                {
+                                    return new Goal(textBoxTitle.Text, richTextBoxDescription.Text, Priority.Important);
+                                }
+                            case 3:
+                                {
+                                    return new Goal(textBoxTitle.Text, richTextBoxDescription.Text, Priority.Important);
+                                }
+                            default:
+                                break; //todo to implement exception
+                        }
+                    }
+                    break;
+                case "Meeting":
+                    {
+                        //start time
+                        int startYear = Convert.ToInt32(comboBoxStartYear.Text);
+                        int startMonth = Convert.ToInt32(comboBoxStartMonth.Text);
+                        int startDay = Convert.ToInt32(comboBoxStartDay.Text);
 
+                        int startHours = Convert.ToInt32(comboBoxStartHrs.Text);
+                        int startMins = Convert.ToInt32(comboBoxStartMins.Text);
+                        int startSecs = Convert.ToInt32(comboBoxStartSecs.Text);
+
+                        DateTime startTime = new DateTime(startYear, startMonth, startDay, startHours, startMins, startSecs);
+
+                        //end time
+                        int endYear = Convert.ToInt32(comboBoxEndYear.Text);
+                        int endMonth = Convert.ToInt32(comboBoxEndMonth.Text);
+                        int endDay = Convert.ToInt32(comboBoxEndDay.Text);
+
+                        int endHours = Convert.ToInt32(comboBoxEndHrs.Text);
+                        int endMins = Convert.ToInt32(comboBoxEndMins.Text);
+                        int endSecs = Convert.ToInt32(comboBoxEndSecs.Text);
+
+                        DateTime endTime = new DateTime(endYear, endMonth, endDay, endHours, endMins, endSecs);
+
+                        return new Meeting(textBoxTitle.Text, richTextBoxDescription.Text, startTime, endTime);
+                    }
+                    break;
+
+                case "Note":
+                    {
+                        return new Note(textBoxTitle.Text, richTextBoxDescription.Text);
+                    }
+                    break;
+
+                case "Task":
+                    {
+                        switch (comboBoxPriority.SelectedIndex)
+                        {
+                            case 0:
+                                break;
+                            case 1:
+                                {
+                                    return new AgendaBackend.Task(textBoxTitle.Text, richTextBoxDescription.Text, Priority.None);
+                                }
+                            case 2:
+                                {
+                                    return new AgendaBackend.Task(textBoxTitle.Text, richTextBoxDescription.Text, Priority.Important);
+                                }
+                            case 3:
+                                {
+                                    return new AgendaBackend.Task(textBoxTitle.Text, richTextBoxDescription.Text, Priority.Important);
+                                }
+                            default:
+                                break; //todo to implement exception
+                        }
+                    }
+                    break;
+                case "Birthday":
+                    {
+                        //start time
+                        int year = Convert.ToInt32(comboBoxStartYear.Text);
+                        int month = Convert.ToInt32(comboBoxStartMonth.Text);
+                        int day = Convert.ToInt32(comboBoxStartDay.Text);
+
+                        DateTime startTime = new DateTime(year, month, day);
+
+                        return new BirthdayReminder(DateTime.Now, textBoxTitle.Text, richTextBoxDescription.Text, richTextBoxDescription.Text, 2,
+                            startTime);
+                    }
+                    break;
+                default:
+                    return null;
+            }
+            return null;
+        }
+
+        private void tabControlBirthDay_Selected(object sender, TabControlEventArgs e)
+        {
+            if (tabControlBirthDay.SelectedIndex.ToString() == "6")
+            {
+                this.Size = new Size(437, 375);
+            }
+            else
+            {
+                this.Size = new Size(792, 375);
+            }
+
+            if (tabControlBirthDay.SelectedIndex.ToString() == "0")
+            {
+                if (this.listOfAgendaObjects.Count > 0)
+                {
+                    foreach (var agendaObject in this.listOfAgendaObjects)
+                    {
+                        this.listBox1.Items.Add(agendaObject);
+                    }
+                }
+            }
+        }
     }
 }
