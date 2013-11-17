967528507965edbaa618ea5c56817704d32b3f67
rstoqnow@gmail.com <rstoqnow@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-17 19:06:36 +0000

    - Updated Windows Forms project: Huge update in the UI and code behind. The UI is ALMOST fully responsive.  - You can select the date you want to view the events for.  - Intuitive UI - resizes by your needs for adding events. There are some MINOR bugs in it:  - The events are shown in the listboxes without clearing the old ones  - The events are NOT show the way the customer would like, but who cares :D (have to override the ToString() method and everything will be okay)  - The size of the form should be calculated more precise There is one MAJOR bug:  - when you set the update interval to 1 minute, if you let the app run for about 3 days (~83 hours) of non-stop work, it will cause overflow exception cause of the callstack (it updates recursively). I really don't think that someone could make it happen but tomorrow I'll check for a better solution. * The main logic is synced with the XAML version. There's nothing new in there for now.

--- trunk/Windows Forms/WFAgenda/Program.cs
+++ trunk/Windows Forms/WFAgenda/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WFAgenda
 {
@@ -16,6 +17,9 @@ namespace WFAgenda
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+               
+        
             Application.Run(new Form1());
         }
     }
