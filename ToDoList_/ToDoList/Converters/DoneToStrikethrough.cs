8b4833652ff6d0378d131096a160fa70ed60a8e9
nmtodorov@gmail.com <nmtodorov@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-22 14:30:54 +0000

    missed files

---
+++ ToDoList_/ToDoList/Converters/DoneToStrikethrough.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoList.Converters
+{
+    using System;
+    using System.Windows.Data;
+    using ToDoList.Models;
+
+    class DoneToStrikethrough:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return "Strikethrough";
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoList_/ToDoList/Models/DataTranslator.cs
@@ -0,0 +1,21 @@
+﻿namespace ToDoList.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    class DataTranslator<T> where T : BaseObjectModel
+    {
+        public static void Serialize(ObservableCollection<T> list)
+        {
+            XmlSerializer seri =
+                new XmlSerializer(typeof(ObservableCollection<T>));
+            using (TextWriter writer = new StreamWriter(@"..\..\serialization_test.xml"))
+            {
+                seri.Serialize(writer, list);
+            }
+        }
+    }
+}
