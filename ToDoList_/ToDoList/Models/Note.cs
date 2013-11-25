5dcd2a6b8954782162e3d3a223f10f646ec34072
kichev.nikolay@gmail.com <kichev.nikolay@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-25 12:13:26 +0000

    Added Note.cs, NoteViewModel.cs and NotesView.xaml.

---
+++ ToDoList_/ToDoList/Models/Note.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Note : BaseObjectModel, INotifyPropertyChanged
+    {
+        private ObservableCollection<Note> Notes;
+        public string Description { get; set; }
+
+        //Constructors
+        public Note(string title, string description) : base(title, description)
+        {
+            this.Notes = new ObservableCollection<Note>();
+        }
+        public Note()
+        {
+        }
+
+        //Methods
+        public void AddNote(Note note)
+        {
+            if (!this.Notes.Contains(note))
+            {
+                this.Notes.Add(note);
+            }
+        }
+        public void RemoveNote(Note note)
+        {
+            if (this.Notes.Contains(note))
+            {
+                this.Notes.Remove(note);
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}", this.Description);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
