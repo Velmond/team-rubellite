adebce461c0e2d7b1ae8d077a0426c4648aea68a
kichev.nikolay@gmail.com <kichev.nikolay@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-11-25 12:13:26 +0000

    Added Note.cs, NoteViewModel.cs and NotesView.xaml.

---
+++ trunk/ToDoList_/ToDoList/ViewModels/NoteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public class NoteViewModel : BaseViewModel<Note>
+    {
+        public NoteViewModel()
+            : base()
+        {
+            this.itemPool = DataTranslator<Note>.Deserialize();
+        }
+    }
+}
---
+++ trunk/ToDoList_/ToDoList/Views/NotesView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToDoList.ViewModels;
+
+namespace ToDoList.Views
+{
+    /// <summary>
+    /// Interaction logic for NotesView.xaml
+    /// </summary>
+    public partial class NotesView : UserControl
+    {
+        public NotesView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            var filterText = this.TextBoxSearch.Text;
+            if (filterText != null)
+            {
+                (this.DataContext as NoteViewModel).Filter(filterText);
+            }
+        }
+    }
+}
