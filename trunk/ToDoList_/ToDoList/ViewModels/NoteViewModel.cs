a68f0aa5b1e2e4d8b9c4d99e5ab5c071e517a5da
mitko.nikoloff@gmail.com <mitko.nikoloff@gmail.com@cb439ef1-8173-c788-fdb4-70e004b91251>
2013-12-08 18:05:19 +0000

    Added sorting for BirthdayReminders and Notes. Added 'Done' check mark for the meetings.

--- trunk/ToDoList_/ToDoList/ViewModels/NoteViewModel.cs
+++ trunk/ToDoList_/ToDoList/ViewModels/NoteViewModel.cs
@@ -1,5 +1,9 @@
 ﻿namespace ToDoList.ViewModels
 {
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Windows.Input;
+    using ToDoList.Commands;
     using ToDoList.Models;
 
     /// <summary>
@@ -13,7 +17,23 @@
         public NoteViewModel()
             : base()
         {
+            this.SortItems = new RelayCommand(this.HandleSortItems);
             this.itemPool = DataTranslator<Note>.Deserialize();
+        }
+
+        /// <summary>
+        /// Gets or sets a command for sorting all notes by their title
+        /// </summary>
+        public ICommand SortItems { get; set; }
+
+        /// <summary>
+        /// Method for handling sorting of all the notes
+        /// </summary>
+        private void HandleSortItems(object obj)
+        {
+            ObservableCollection<Note> sorted = new ObservableCollection<Note>();
+            sorted = new ObservableCollection<Note>(this.itemPool.OrderBy(note => note.Title));
+            this.Items = sorted;
         }
     }
 }
